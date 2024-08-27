75c0cabf92aef9a2bdcff592458b237d27dfe1f7
giangann <70595717+giangann@users.noreply.github.com>
2024-08-27 14:27:18 +0700

    form input, useController

--- app/(tabs)/settings/out_of_working_time_types/add-out-of-working-time-type.tsx
+++ app/(tabs)/settings/out_of_working_time_types/add-out-of-working-time-type.tsx
@@ -1,7 +1,7 @@
-import { BaseInput } from "@/components/BaseInput";
+import { FormInput } from "@/components/FormInput";
 import { ThemedText } from "@/components/ThemedText";
-import { Controller, useForm, useController } from "react-hook-form";
-import { Button, Image, Text, TouchableOpacity, View } from "react-native";
+import { useForm } from "react-hook-form";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import Toast from "react-native-root-toast";
 const LeaveTypeIconLeft = require("@/assets/images/identify-card.png");
 
@@ -10,17 +10,10 @@ type CreateItem = {
 };
 
 export default function AddOutOfWorkingTimeType() {
-  const { control, getValues, handleSubmit } = useForm<CreateItem>({ defaultValues: { name: "" } });
-  const { field } = useController({ name: "name", control: control });
+  const { control, handleSubmit, setError } = useForm<CreateItem>({ defaultValues: { name: "" } });
 
   const onCreate = async (data: CreateItem) => {
-    console.log("call post api");
-
-    const values = getValues();
-
     console.log("data", data);
-
-    console.log("values", values);
 
     const toastEl = (
       <>
@@ -31,32 +24,22 @@ export default function AddOutOfWorkingTimeType() {
 
     // @ts-ignore
     Toast.show(toastEl, { shadow: false, backgroundColor: "green" });
+
+    setError("name", { message: "This field is required" });
   };
 
   return (
     <View>
       <ThemedText type="title">Tạo loại ngoài giờ</ThemedText>
-      {/* <Controller
-        control={control}
-        name="name"
-        render={({ field: { onChange, onBlur, value } }) => (
-          <BaseInput
-            placeholder="Nhập tên loại nghỉ..."
-            leftIconImage={LeaveTypeIconLeft}
-            rightIconImage={LeaveTypeIconLeft}
-            value={value}
-            onChangeText={onChange}
-            onBlur={onBlur}
-          />
-        )}
-      /> */}
-      <BaseInput
+      <FormInput
+        formInputProps={{ control: control, name: "name" }}
+        label="Tên loại nghỉ"
+        required
         placeholder="Nhập tên loại nghỉ..."
         leftIconImage={LeaveTypeIconLeft}
         rightIconImage={LeaveTypeIconLeft}
-        value={field.value}
-        onChangeText={field.onChange}
       />
+
       <TouchableOpacity activeOpacity={0.8} onPress={handleSubmit(onCreate)}>
         <View style={{ justifyContent: "center", alignItems: "center", backgroundColor: "#0B3A82", height: 40, width: "100%", borderRadius: 4 }}>
           <Text style={{ fontFamily: "Nunito", color: "white", fontWeight: 600, fontSize: 14 }}>Tạo</Text>
--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -1,83 +1,149 @@
 import { OPACITY_TO_HEX } from "@/constants/Colors";
-import { Text, TextInput, type TextProps, type TextInputProps, StyleSheet, View, Image, TouchableHighlight } from "react-native";
-import { type UseFormReturn, type FieldValues } from "react-hook-form";
+import React from "react";
+import { UseControllerProps, useController, type FieldValues } from "react-hook-form";
+import { Image, ImageStyle, StyleSheet, Text, TextInput, TextStyle, TouchableHighlight, View, ViewStyle, type TextInputProps } from "react-native";
+const XClearIconImage = require("@/assets/images/x-clear.png");
+
 export type FormInputProps<T extends FieldValues> = {
-  formInputProps: UseFormReturn<T>;
+  formInputProps: UseControllerProps<T>;
 };
 
 export type BaseInputProps<T extends FieldValues> = TextInputProps &
   FormInputProps<T> & {
     required?: boolean;
-    error?: string;
+    label?: string;
     leftIconImage: any;
     rightIconImage: any;
     rightIconEl?: React.ReactNode;
   };
 
-export function FormInput<T extends FieldValues>({ placeholder, leftIconImage, rightIconImage, rightIconEl, error }: BaseInputProps<T>) {
+export function FormInput<T extends FieldValues>({
+  label,
+  required,
+  leftIconImage,
+  rightIconImage,
+  rightIconEl,
+  formInputProps,
+  ...rest
+}: BaseInputProps<T>) {
+  const { field, fieldState } = useController(formInputProps);
+  const { value, onChange } = field;
+  const { error } = fieldState;
   return (
-    <View style={{ position: "relative", marginBottom: 8 }}>
-      {/* input */}
-      <TextInput placeholder={placeholder} style={inputStyles.default} />
+    <View>
+      {/* label */}
+      <Text>
+        {label} {required && <span style={{ color: "red" }}>*</span>}
+      </Text>
 
-      {/* left icon */}
-      <View style={{ position: "absolute", top: 0, left: 0, width: 36, height: "100%" }}>
-        <View style={{ flex: 1, width: "100%", height: "100%", justifyContent: "center", alignItems: "center" }}>
-          <Image source={leftIconImage} style={imageStyles.icon} />
+      {/* input container */}
+      <View style={{ position: "relative", marginBottom: 8 }}>
+        {/* input */}
+        <TextInput
+          placeholderTextColor={`#000000${OPACITY_TO_HEX["50"]}`}
+          value={value}
+          onChangeText={onChange}
+          style={error ? inputStyles.error : inputStyles.default}
+          {...rest}
+        />
+
+        {/* left icon */}
+        <View style={{ position: "absolute", top: 0, left: 0, width: 36, height: "100%" }}>
+          <View style={{ flex: 1, width: "100%", height: "100%", justifyContent: "center", alignItems: "center" }}>
+            <Image source={leftIconImage} style={{ ...imageStyles.icon }} />
+          </View>
+        </View>
+
+        {/* right icon */}
+        <View style={{ position: "absolute", top: 0, right: 0, width: 36, height: "100%" }}>
+          <View style={{ flex: 1, width: "100%", height: "100%", justifyContent: "center", alignItems: "center" }}>
+            {rightIconEl && rightIconEl}
+            {!rightIconEl && (
+              <TouchableHighlight
+                onPress={() => {
+                  /**clear value input */
+                  onChange("");
+                }}
+              >
+                <View style={{ width: 18, height: 18 }}>
+                  <Image source={XClearIconImage} style={{ ...imageStyles.icon, opacity: value === "" ? 0.5 : 1 }} />
+                </View>
+              </TouchableHighlight>
+            )}
+          </View>
         </View>
       </View>
-
-      {/* right icon */}
-      <View style={{ position: "absolute", top: 0, right: 0, width: 36, height: "100%" }}>
-        <View style={{ flex: 1, width: "100%", height: "100%", justifyContent: "center", alignItems: "center" }}>
-          {rightIconEl && rightIconEl}
-          {!rightIconEl && (
-            <TouchableHighlight>
-              <View style={{ width: 18, height: 18 }}>
-                <Image source={rightIconImage} style={imageStyles.icon} />
-              </View>
-            </TouchableHighlight>
-          )}
-        </View>
-      </View>
-
-      {error && <Text style={{ color: "red" }}>{error}</Text>}
+      {Boolean(error) && <Text style={{ color: "red" }}>{error?.message}</Text>}
     </View>
   );
 }
 
+// export const FormInput = React.memo(function FormInput<T extends FieldValues>({
+//   placeholder,
+//   leftIconImage,
+//   rightIconImage,
+//   rightIconEl,
+//   formInputProps,
+// }: BaseInputProps<T>) {
+//   const { field, formState } = useController(formInputProps);
+//   const error = formState.errors[field.name];
+//   return (
+//     <View style={{ position: "relative", marginBottom: 8 }}>
+//       {/* input */}
+//       <TextInput
+//         placeholder={placeholder}
+//         style={formState.errors[field.name] ? inputStyles.error : inputStyles.default}
+//         value={field.value}
+//         onChangeText={field.onChange}
+//       />
+
+//       {/* left icon */}
+//       <View style={{ position: "absolute", top: 0, left: 0, width: 36, height: "100%" }}>
+//         <View style={{ flex: 1, width: "100%", height: "100%", justifyContent: "center", alignItems: "center" }}>
+//           <Image source={leftIconImage} style={imageStyles.icon} />
+//         </View>
+//       </View>
+
+//       {/* right icon */}
+//       <View style={{ position: "absolute", top: 0, right: 0, width: 36, height: "100%" }}>
+//         <View style={{ flex: 1, width: "100%", height: "100%", justifyContent: "center", alignItems: "center" }}>
+//           {rightIconEl && rightIconEl}
+//           {!rightIconEl && (
+//             <TouchableHighlight>
+//               <View style={{ width: 18, height: 18 }}>
+//                 <Image source={rightIconImage} style={imageStyles.icon} />
+//               </View>
+//             </TouchableHighlight>
+//           )}
+//         </View>
+//       </View>
+
+//       {Boolean(error) && <Text style={{ color: "red" }}>{error?.message as string}</Text>}
+//     </View>
+//   );
+// });
+
+const baseInputStyle: ViewStyle | TextStyle | ImageStyle = {
+  padding: 10,
+  paddingLeft: 36,
+  paddingRight: 36,
+  borderWidth: 1,
+  height: 40,
+  borderRadius: 4,
+  fontFamily: "Nunito",
+};
+
 const inputStyles = StyleSheet.create({
   default: {
-    padding: 10,
-    paddingLeft: 36,
-    paddingRight: 36,
-    borderWidth: 1,
-    height: 40,
-    borderRadius: 4,
-    fontFamily: "Nunito",
-
+    ...baseInputStyle,
     borderColor: `#000000${OPACITY_TO_HEX["20"]}`,
   },
   focus: {
-    padding: 10,
-    paddingLeft: 36,
-    paddingRight: 36,
-    borderWidth: 1,
-    height: 40,
-    borderRadius: 4,
-    fontFamily: "Nunito",
-
-    borderColor: `#000000`,
+    ...baseInputStyle,
+    borderColor: `pink`,
   },
   error: {
-    padding: 10,
-    paddingLeft: 36,
-    paddingRight: 36,
-    borderWidth: 1,
-    height: 40,
-    borderRadius: 4,
-    fontFamily: "Nunito",
-
+    ...baseInputStyle,
     borderColor: `red`,
   },
 });
