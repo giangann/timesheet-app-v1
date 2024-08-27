8b80bca6a393a75a841e0dc54efc039b2622b169
giangann <70595717+giangann@users.noreply.github.com>
2024-08-27 11:54:07 +0700

    hook form setup

--- app/(tabs)/settings/out_of_working_time_types/add-out-of-working-time-type.tsx
+++ app/(tabs)/settings/out_of_working_time_types/add-out-of-working-time-type.tsx
@@ -1,12 +1,26 @@
 import { BaseInput } from "@/components/BaseInput";
 import { ThemedText } from "@/components/ThemedText";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Controller, useForm, useController } from "react-hook-form";
+import { Button, Image, Text, TouchableOpacity, View } from "react-native";
 import Toast from "react-native-root-toast";
 const LeaveTypeIconLeft = require("@/assets/images/identify-card.png");
 
+type CreateItem = {
+  name: string;
+};
+
 export default function AddOutOfWorkingTimeType() {
-  const onCreate = async () => {
+  const { control, getValues, handleSubmit } = useForm<CreateItem>({ defaultValues: { name: "" } });
+  const { field } = useController({ name: "name", control: control });
+
+  const onCreate = async (data: CreateItem) => {
     console.log("call post api");
+
+    const values = getValues();
+
+    console.log("data", data);
+
+    console.log("values", values);
 
     const toastEl = (
       <>
@@ -18,12 +32,32 @@ export default function AddOutOfWorkingTimeType() {
     // @ts-ignore
     Toast.show(toastEl, { shadow: false, backgroundColor: "green" });
   };
+
   return (
     <View>
       <ThemedText type="title">Tạo loại ngoài giờ</ThemedText>
-      <BaseInput placeholder="Nhập tên loại nghỉ..." leftIconImage={LeaveTypeIconLeft} rightIconImage={LeaveTypeIconLeft} />
-
-      <TouchableOpacity activeOpacity={0.8} onPress={onCreate}>
+      {/* <Controller
+        control={control}
+        name="name"
+        render={({ field: { onChange, onBlur, value } }) => (
+          <BaseInput
+            placeholder="Nhập tên loại nghỉ..."
+            leftIconImage={LeaveTypeIconLeft}
+            rightIconImage={LeaveTypeIconLeft}
+            value={value}
+            onChangeText={onChange}
+            onBlur={onBlur}
+          />
+        )}
+      /> */}
+      <BaseInput
+        placeholder="Nhập tên loại nghỉ..."
+        leftIconImage={LeaveTypeIconLeft}
+        rightIconImage={LeaveTypeIconLeft}
+        value={field.value}
+        onChangeText={field.onChange}
+      />
+      <TouchableOpacity activeOpacity={0.8} onPress={handleSubmit(onCreate)}>
         <View style={{ justifyContent: "center", alignItems: "center", backgroundColor: "#0B3A82", height: 40, width: "100%", borderRadius: 4 }}>
           <Text style={{ fontFamily: "Nunito", color: "white", fontWeight: 600, fontSize: 14 }}>Tạo</Text>
         </View>
--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -1,19 +1,24 @@
 import { OPACITY_TO_HEX } from "@/constants/Colors";
 import { Text, TextInput, type TextProps, type TextInputProps, StyleSheet, View, Image, TouchableHighlight } from "react-native";
-
-export type BaseInputProps = TextInputProps & {
-  required?: boolean;
-  error?: string;
-  leftIconImage: any;
-  rightIconImage: any;
-  rightIconEl?: React.ReactNode;
+import { type UseFormReturn, type FieldValues } from "react-hook-form";
+export type FormInputProps<T extends FieldValues> = {
+  formInputProps: UseFormReturn<T>;
 };
 
-export function BaseInput({ placeholder, leftIconImage, rightIconImage, rightIconEl, error, ...rest }: BaseInputProps) {
+export type BaseInputProps<T extends FieldValues> = TextInputProps &
+  FormInputProps<T> & {
+    required?: boolean;
+    error?: string;
+    leftIconImage: any;
+    rightIconImage: any;
+    rightIconEl?: React.ReactNode;
+  };
+
+export function FormInput<T extends FieldValues>({ placeholder, leftIconImage, rightIconImage, rightIconEl, error }: BaseInputProps<T>) {
   return (
     <View style={{ position: "relative", marginBottom: 8 }}>
       {/* input */}
-      <TextInput placeholder={placeholder} style={inputStyles.default} {...rest} />
+      <TextInput placeholder={placeholder} style={inputStyles.default} />
 
       {/* left icon */}
       <View style={{ position: "absolute", top: 0, left: 0, width: 36, height: "100%" }}>
