ceffe45598c8246b1db36645e1eb2e987f526003
giangann <70595717+giangann@users.noreply.github.com>
2024-08-30 22:46:21 +0700

    create new holiday

--- app/(tabs)/settings/holidays/add-holiday.tsx
+++ app/(tabs)/settings/holidays/add-holiday.tsx
@@ -1,13 +1,21 @@
 import { FormInput } from "@/components/FormInput";
 import { FormPickDate } from "@/components/FormPickDate";
 import { FormSelect } from "@/components/FormSelect";
-import { ThemedText } from "@/components/ThemedText";
 import { NunitoText } from "@/components/text/NunitoText";
+import { useSession } from "@/contexts/ctx";
+import { useFocusEffect, useRouter } from "expo-router";
 import moment from "moment";
+import { useCallback, useState } from "react";
 import { useForm } from "react-hook-form";
-import { ScrollView, Image, Text, TouchableOpacity, View, StyleSheet } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Toast, { ToastOptions } from "react-native-root-toast";
 const LeaveTypeIconLeft = require("@/assets/images/identify-card.png");
 
+type TSalaryCoefficientType = {
+  id: number;
+  name: string;
+  coefficient: number;
+};
 type CreateItemForm = {
   name: string;
   date: string | Date; // YYYY-MM-DD
@@ -23,9 +31,98 @@ type CreateItem = {
 export default function CreateHoliday() {
   const { control, handleSubmit } = useForm<CreateItemForm>({ defaultValues: { name: "", date: new Date() } });
 
+  const [salaryCoefficientTypes, setSalaryCoefficientTypes] = useState<TSalaryCoefficientType[]>([]);
+  const { session } = useSession();
+  const router = useRouter();
+
+  const salaryCoefficientTypeOptions = salaryCoefficientTypes.map((type) => ({
+    value: type.id,
+    label: type.name,
+  }));
+
+  const fetchSalaryCoefTypes = async () => {
+    const token = `Bearer ${session}` ?? "xxx";
+
+    const baseUrl = "http://13.228.145.165:8080/api/v1";
+    const endpoint = "/salary-coefficient-types";
+    const url = `${baseUrl}${endpoint}`;
+
+    const response = await fetch(url, {
+      method: "GET",
+      headers: { "Content-Type": "application/json", Authorization: token },
+      credentials: "include",
+    });
+    const responseJson = await response.json();
+
+    if (responseJson.statusCode === 200) {
+      setSalaryCoefficientTypes(responseJson.data.salaryCoefficientTypes);
+    } else {
+      let toastEl: any = null;
+      let toastOptions: ToastOptions;
+      toastEl = (
+        <>
+          <NunitoText lightColor="white" type="body3">
+            {responseJson.error}
+          </NunitoText>
+        </>
+      );
+      toastOptions = {
+        backgroundColor: "#C84851",
+      };
+    }
+  }
+
+  useFocusEffect(
+    useCallback(()=>{
+      fetchSalaryCoefTypes()
+    },[])
+  );
+
   const onCreate = async (data: CreateItemForm) => {
-    const bodyData = { ...data, date: moment(data.date).format("YYYY-MM-DD") };
+    const bodyData: CreateItem = { ...data, date: moment(data.date).format("YYYY-MM-DD") };
     console.log("data form", bodyData);
+
+    const token = `Bearer ${session}` ?? "xxx";
+    const baseUrl = "http://13.228.145.165:8080/api/v1";
+    const endpoint = "/holidays";
+    const url = `${baseUrl}${endpoint}`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json", Authorization: token },
+      body: JSON.stringify(bodyData),
+      credentials: "include",
+    });
+    const responseJson = await response.json();
+
+    let toastEl: any = null;
+    let toastOptions: ToastOptions;
+    if (responseJson.statusCode === 200) {
+      toastEl = (
+        <>
+          <Image style={{ backgroundColor: "white" }} source={LeaveTypeIconLeft} />
+          <Text>{"create success"}</Text>
+        </>
+      );
+      toastOptions = {
+        backgroundColor: "green",
+      };
+
+      router.back();
+    } else {
+      toastEl = (
+        <>
+          <Image style={{ backgroundColor: "white" }} source={LeaveTypeIconLeft} />
+          <Text>{responseJson.error}</Text>
+        </>
+      );
+      toastOptions = {
+        backgroundColor: "red",
+      };
+    }
+
+    // @ts-ignore
+    Toast.show(toastEl, toastOptions);
   };
   return (
     <View style={styles.container}>
@@ -40,7 +137,6 @@ export default function CreateHoliday() {
           // rightIconImage={LeaveTypeIconLeft}
         />
 
-        {/* Add more FormInput components as needed */}
         <FormInput
           formInputProps={{ control: control, name: "name" }}
           label="Tên ngày nghỉ"
@@ -52,11 +148,22 @@ export default function CreateHoliday() {
 
         <FormSelect
           useControllerProps={{ control: control, name: "salaryCoefficientTypeId" }}
-          options={options}
+          options={salaryCoefficientTypeOptions}
           label="Loại ngoài giờ"
           required
           placeholder="Chọn loại ngoài giờ"
         />
+        <FormSelect
+          useControllerProps={{ control: control, name: "activeOutsideWorkingTime" }}
+          options={[
+            { value: false, label: "Không cho phép" },
+            { value: true, label: "Cho phép" },
+          ]}
+          label="Cho phép ngoài giờ"
+          required
+          placeholder="Lựa chọn cho phép / không cho phép ngoài giờ"
+        />
+        {/* Add more FormInput components as needed */}
       </ScrollView>
       <TouchableOpacity activeOpacity={0.8} onPress={handleSubmit(onCreate)} style={styles.buttonContainer}>
         <View style={styles.button}>
@@ -96,20 +203,3 @@ const styles = StyleSheet.create({
     borderRadius: 4,
   },
 });
-
-type TOption = {
-  value: any;
-  label: string;
-} & Record<string, unknown>;
-const options: TOption[] = [
-  {
-    value: 2,
-    label: "Làm ngoài giờ ngày cuối tuần",
-    sallaryCoefId: 1,
-  },
-  {
-    value: 3,
-    label: "Làm ngoài giờ ngày lễ",
-    sallaryCoefId: 2,
-  },
-];
