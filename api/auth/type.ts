17dc4bffe77c37731f9bd964af75c1c049d43f00
giangann <70595717+giangann@users.noreply.github.com>
2024-09-08 12:00:45 +0700

    update type

--- api/auth/type.ts
+++ api/auth/type.ts
@@ -2,3 +2,13 @@ export type TCredentials = {
   identifyCard: string;
   password: string;
 };
+
+export type TUserInfo = {
+  address: string;
+  email: string;
+  identifyCard: string;
+  name: string;
+  phone: string;
+  roleCode: string;
+  roleName: string;
+};
--- constants/Misc.ts
+++ constants/Misc.ts
@@ -12,3 +12,21 @@ export const FORM_STATUS_NAME = {
   [FORM_STATUS.ACCEPTED]: "Chấp thuận",
   [FORM_STATUS.REJECTED]: "Từ chối",
 };
+
+
+// const SAMPLE_RESPONSE_JSON = {
+//   data: {
+//     user: {
+//       address: "No add",
+//       email: "dmc@gmail.com",
+//       identifyCard: "000000001111",
+//       name: "Đặng Minh Chính",
+//       phone: "00000000000",
+//       roleCode: "TEAM_DIRECTOR",
+//       roleName: "Lãnh đạo phòng",
+//     },
+//   },
+//   status: "OK",
+//   statusCode: 200,
+//   timestamp: "2024-09-08T11:57:19.672520792",
+// };
