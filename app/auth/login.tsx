01423c49d28f53b7a67eac3965c998c5cbad14bf
giangann <70595717+giangann@users.noreply.github.com>
2024-09-09 12:48:36 +0700

    update screen login: keyboardAvodingView

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -9,7 +9,7 @@ import Feather from "@expo/vector-icons/Feather";
 import { router, useNavigation } from "expo-router";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { Image, Pressable, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import { Image, Keyboard, Pressable, SafeAreaView, ScrollView, Text, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
 import * as Progress from "react-native-progress";
 import { StyleSheet, KeyboardAvoidingView, Platform } from "react-native";
 import { NunitoText } from "@/components/text/NunitoText";
@@ -51,49 +51,57 @@ const Login = () => {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={{ flex: 1 }} keyboardVerticalOffset={800}>
-        <View style={styles.container}>
-          <View style={styles.logoWrapper}>
-            <Image source={LoginBanner} style={{ opacity: 0.5 }} />
-          </View>
-
-          <View style={styles.formWrapper}>
-            <NunitoText style={{ textAlign: "center" }}>Đăng nhập</NunitoText>
-
-            <View style={styles.fieldWrapper}>
-              {/* Input_1 - Identify Card  */}
-              <FormInput
-                formInputProps={{ control: control, name: "identifyCard" }}
-                placeholder="Nhập số CCCD..."
-                leftIcon={<AntDesign name="idcard" size={18} color={Colors.light.inputIconNone} />}
-              />
-              {/* Input_2 - Password */}
-              <FormInput
-                formInputProps={{ control: control, name: "password" }}
-                secureTextEntry={!showPw}
-                placeholder="Nhập mật khẩu..."
-                leftIcon={<MaterialIcons name="password" size={18} color={Colors.light.inputIconNone} />}
-                rightIconEl={
-                  <Pressable onPress={onToggleShowPw}>
-                    <View style={{ padding: 8 }}>
-                      <Feather name={showPw ? "eye-off" : "eye"} size={18} color={Colors.light.inputIconNone} />
-                    </View>
-                  </Pressable>
-                }
-              />
-            </View>
-
-            {/* Button - Login */}
-            <TouchableOpacity activeOpacity={0.8} onPress={handleSubmit(onLogin)} disabled={isSubmitting}>
-              <View style={styles.btnInner}>
-                {isSubmitting && <Progress.Circle indeterminate size={14} />}
-                <NunitoText type="body3" lightColor="white">
-                  Đăng nhập
-                </NunitoText>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "android" ? "height" : "padding"}
+        style={{ flex: 1 }}
+        keyboardVerticalOffset={0} // Adjust based on your testing
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+            <View style={styles.container}>
+              <View style={styles.logoWrapper}>
+                <Image source={LoginBanner} style={{ opacity: 0.5 }} />
               </View>
-            </TouchableOpacity>
-          </View>
-        </View>
+
+              <View style={styles.formWrapper}>
+                <NunitoText style={{ textAlign: "center" }}>Đăng nhập</NunitoText>
+
+                <View style={styles.fieldWrapper}>
+                  {/* Input_1 - Identify Card */}
+                  <FormInput
+                    formInputProps={{ control: control, name: "identifyCard" }}
+                    placeholder="Nhập số CCCD..."
+                    leftIcon={<AntDesign name="idcard" size={18} color={Colors.light.inputIconNone} />}
+                  />
+                  {/* Input_2 - Password */}
+                  <FormInput
+                    formInputProps={{ control: control, name: "password" }}
+                    secureTextEntry={!showPw}
+                    placeholder="Nhập mật khẩu..."
+                    leftIcon={<MaterialIcons name="password" size={18} color={Colors.light.inputIconNone} />}
+                    rightIconEl={
+                      <Pressable onPress={onToggleShowPw}>
+                        <View style={{ padding: 8 }}>
+                          <Feather name={showPw ? "eye-off" : "eye"} size={18} color={Colors.light.inputIconNone} />
+                        </View>
+                      </Pressable>
+                    }
+                  />
+                </View>
+
+                {/* Button - Login */}
+                <TouchableOpacity activeOpacity={0.8} onPress={handleSubmit(onLogin)} disabled={isSubmitting}>
+                  <View style={styles.btnInner}>
+                    {isSubmitting && <Progress.Circle indeterminate size={14} />}
+                    <NunitoText type="body3" lightColor="white">
+                      Đăng nhập
+                    </NunitoText>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </ScrollView>
+        </TouchableWithoutFeedback>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
