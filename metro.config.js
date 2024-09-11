a75108a066ec64debc66db545da9e5a470c22242
giangann <70595717+giangann@users.noreply.github.com>
2024-09-11 16:35:54 +0700

    reset metro config, uninstall react native svg transformer

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,3 @@
 const { getSentryExpoConfig } = require("@sentry/react-native/metro");
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
-const defaultConfig = getDefaultConfig(__dirname);
 const config = getSentryExpoConfig(__dirname);
-
-const svgConfig = {
-  transformer: {
-    babelTransformerPath: require.resolve("react-native-svg-transformer"),
-  },
-  resolver: {
-    assetExts: defaultConfig.resolver.assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...defaultConfig.resolver.sourceExts, "svg"],
-  },
-};
-
-module.exports = mergeConfig(config, svgConfig);
+module.exports = config;
