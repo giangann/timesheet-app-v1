006ecf46a065b9c577be3227de18e6502c64f92e
giangann <70595717+giangann@users.noreply.github.com>
2024-09-12 20:22:18 +0700

    update locale calendar

--- configs/rnCalendarLocalConfig.ts
+++ configs/rnCalendarLocalConfig.ts
@@ -1,23 +1,12 @@
 import { LocaleConfig } from "react-native-calendars";
-LocaleConfig.locales['fr'] = {
-    monthNames: [
-      'Janvier',
-      'Février',
-      'Mars',
-      'Avril',
-      'Mai',
-      'Juin',
-      'Juillet',
-      'Août',
-      'Tháng 9',
-      'Octobre',
-      'Novembre',
-      'Décembre'
-    ],
-    monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
-    dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
-    dayNamesShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
-    today: "Aujourd'hui"
-  };
-  LocaleConfig.defaultLocale = 'fr';
-  
+
+LocaleConfig.locales["vi"] = {
+  monthNames: ["Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6", "Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"],
+  monthNamesShort: ["Thg 1", "Thg 2", "Thg 3", "Thg 4", "Thg 5", "Thg 6", "Thg 7", "Thg 8", "Thg 9", "Thg 10", "Thg 11", "Thg 12"],
+  dayNames: ["Chủ Nhật", "Thứ Hai", "Thứ Ba", "Thứ Tư", "Thứ Năm", "Thứ Sáu", "Thứ Bảy"],
+  dayNamesShort: ["CN", "T2", "T3", "T4", "T5", "T6", "T7"],
+  today: "Hôm nay",
+};
+
+// Set default locale to Vietnamese
+LocaleConfig.defaultLocale = "vi";
