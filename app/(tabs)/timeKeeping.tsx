bcd56c5e3e8aa61cbc0ddeffe1f465a99bec530c
giangann <70595717+giangann@users.noreply.github.com>
2024-09-12 17:45:52 +0700

    timeKeeping - update default page

--- app/(tabs)/timeKeeping.tsx
+++ app/(tabs)/timeKeeping.tsx
@@ -1,18 +1,70 @@
 import { NunitoText } from "@/components/text/NunitoText";
+import { OPACITY_TO_HEX } from "@/constants/Colors";
+import { UNIT_DIMENSION } from "@/constants/Misc";
 import { useRouter } from "expo-router";
-import { Button, StyleSheet, View } from "react-native";
+import { Image, Pressable, ScrollView, StyleSheet, View } from "react-native";
+
+const SettingTeamIconImage = require("@/assets/images/setting-team.png");
+const GotoDetailIconImage = require("@/assets/images/gotodetail.png");
 
 export default function TimeKeeping() {
   const router = useRouter();
   return (
     <View style={styles.container}>
-      <NunitoText>TIME KEEPING DEFAULT SCREEN</NunitoText>
-
-      <Button title="to Today Time Keeping" onPress={() => router.push("/time-keepings/today-time-keeping")} />
+      <ScrollView contentContainerStyle={styles.listBox}>
+        <Pressable onPress={() => router.push("/time-keepings/today-time-keeping")}>
+          <View style={styles.itemBox}>
+            <View style={styles.itemBoxLeft}>
+              <View style={styles.iconBox}>
+                <Image source={SettingTeamIconImage} />
+              </View>
+              <NunitoText type="body1">Chấm công hôm nay</NunitoText>
+            </View>
+            <View style={styles.chip}>
+              <Image source={GotoDetailIconImage} />
+            </View>
+          </View>
+        </Pressable>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    padding: 16,
+    paddingBottom: 0,
+    backgroundColor: "white",
+    minHeight: "100%",
+  },
+  listBox: {
+    paddingBottom: 16,
+    gap: 20 * UNIT_DIMENSION,
+  },
+  itemBox: {
+    backgroundColor: `#0B3A82${OPACITY_TO_HEX["15"]}`,
+    paddingHorizontal: 16 * UNIT_DIMENSION,
+    paddingVertical: 12 * UNIT_DIMENSION,
+    borderRadius: 8 * UNIT_DIMENSION,
+
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  itemBoxLeft: {
+    flexBasis: "60%",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12 * UNIT_DIMENSION,
+  },
+  iconBox: {
+    backgroundColor: `#0B3A82`,
+    padding: 8 * UNIT_DIMENSION,
+    borderRadius: 8 * UNIT_DIMENSION,
+  },
+  chip: {
+    borderRadius: 16 * UNIT_DIMENSION,
+    backgroundColor: `#0B3A82`,
+    padding: 6 * UNIT_DIMENSION,
+  },
 });
