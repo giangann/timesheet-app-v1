9ac62a173dfa313fc01a39c5e16aea6cfedcab94
giangann <70595717+giangann@users.noreply.github.com>
2024-09-24 17:23:42 +0700

    websocket by socket.io-client

--- app/notification/noti.tsx
+++ app/notification/noti.tsx
@@ -1,37 +1,19 @@
 import { NunitoText } from "@/components/text/NunitoText";
 import { useEffect } from "react";
 import { StyleSheet, View } from "react-native";
-import { WebSocket } from "ws";
-
+import { io } from "socket.io-client";
 export default function Noti() {
   useEffect(() => {
-    const wsClient = new WebSocket("wss://proven-incredibly-redbird.ngrok-free.app/api/v1/ws");
+    const wsClient = io("https://proven-incredibly-redbird.ngrok-free.app/ws");
 
-    const onOpen = () => {
-      console.log("connection opened");
-    };
-    const onClose = (ev: WebSocketEventMap["close"]) => {
-      console.log("connection closed:", ev.timeStamp);
-    };
-    const onMessage = (ev: WebSocketMessageEvent) => {
-      console.log("new event: ", ev);
-      console.log("event info:", ev.target, ev.data);
-    };
-    const onError = (ev: WebSocketMessageEvent) => {
-      console.log("error occured: ", ev);
-    };
+    wsClient.on("connect", () => {
+      console.log("connected success");
+    });
 
-    wsClient.on("open", onOpen);
-    wsClient.on("close", onClose);
-    wsClient.on("message", onMessage);
-    wsClient.on("error", onError);
+    console.log("wsClient id: ", wsClient.id);
 
     return () => {
-      wsClient.close();
-      wsClient.off("open", onOpen);
-      wsClient.off("close", onClose);
-      wsClient.off("message", onMessage);
-      wsClient.off("error", onError);
+      wsClient.off("connect");
     };
   }, []);
   return (
