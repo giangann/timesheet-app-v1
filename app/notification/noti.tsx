73a4188d19d851984d4216b4d3c6b49e2ec9d4de
giangann <70595717+giangann@users.noreply.github.com>
2024-09-24 17:50:58 +0700

    websocket return used by built-in react-native websocket, rm redudant packages

--- app/notification/noti.tsx
+++ app/notification/noti.tsx
@@ -1,19 +1,46 @@
 import { NunitoText } from "@/components/text/NunitoText";
 import { useEffect } from "react";
 import { StyleSheet, View } from "react-native";
-import { io } from "socket.io-client";
+const token =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhZGRyZXNzIjoiTm8gYWRkIiwicm9sZSI6IkzDo25oIMSR4bqhbyBwaMOybmciLCJwaG9uZSI6IjAwMDAwMDAwMDAwIiwibmFtZSI6IsSQ4bq3bmcgTWluaCBDaMOtbmgiLCJlbWFpbCI6ImRtY0BnbWFpbC5jb20iLCJzdWIiOiIwMDAwMDAwMDExMTEiLCJpYXQiOjE3MjcxNjkxNDcsImV4cCI6MTcyNzI1NTU0N30.L0hdG6s_UVv3jaHNsZ3AZgoYV672f9OHyALc1hNvYps";
 export default function Noti() {
   useEffect(() => {
-    const wsClient = io("https://proven-incredibly-redbird.ngrok-free.app/ws");
+    const wsClient = new WebSocket("wss://proven-incredibly-redbird.ngrok-free.app/ws");
 
-    wsClient.on("connect", () => {
-      console.log("connected success");
-    });
+    const onOpen = () => {
+      console.log("connection opened");
 
-    console.log("wsClient id: ", wsClient.id);
+      const authMessage = { type: "AUTH", token: token };
+      // wsClient.send(JSON.stringify(authMessage));
+    };
+
+    const onClose = (event: WebSocketCloseEvent) => {
+      console.log(`WebSocket connection closed, code=${event.code}, reason=${event.reason}, message=${event.message}`);
+    };
+
+    const onMessage = (ev: WebSocketMessageEvent) => {
+      const evDataParsed = JSON.parse(ev.data);
+      console.log("new event: ", ev);
+      console.log("event target:", ev.target);
+      console.log("event data:", ev.data);
+      console.log("event data type: ", evDataParsed.type);
+      console.log("event is trusted: ", ev.isTrusted);
+    };
+    const onError = (ev: WebSocketMessageEvent) => {
+      console.log("error occured: ", ev);
+    };
+
+    wsClient.onopen = onOpen;
+    wsClient.onclose = onClose;
+    wsClient.onmessage = onMessage;
+    wsClient.onerror = onError;
 
     return () => {
-      wsClient.off("connect");
+      wsClient.close();
+      wsClient.removeEventListener("open", onOpen);
+      wsClient.removeEventListener("close", onClose);
+      wsClient.removeEventListener("message", onMessage);
+      wsClient.removeEventListener("error", onError);
     };
   }, []);
   return (
