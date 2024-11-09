e9cb6363d90f989d7470b6e17f558bc3772287f3
giangann <70595717+giangann@users.noreply.github.com>
2024-11-09 20:42:26 +0700

    add theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import { PaperProvider } from "react-native-paper";
 import { SessionProvider } from "@/contexts/ctx";
 import { useColorScheme } from "@/hooks/useColorScheme.web";
 import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
@@ -9,6 +10,7 @@ import "../configs/rnCalendarLocalConfig";
 import * as Sentry from "@sentry/react-native";
 import { NunitoText } from "@/components/text/NunitoText";
 import { SocketProvider } from "@/contexts/socket-ctx";
+import { reactNativePaperCustomTheme } from "@/configs/rnp-custom-theme";
 
 Sentry.init({
   dsn: "https://449a5848754654eceaa1424ad7da4636@o4507923809173504.ingest.us.sentry.io/4507928784732160",
@@ -36,53 +38,55 @@ function Root() {
   return (
     <RootSiblingParent>
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <SessionProvider>
-          <SocketProvider>
-            <Stack>
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-              <Stack.Screen name="forms" options={{ headerShown: false }} />
-              <Stack.Screen name="settings" options={{ headerShown: false }} />
-              <Stack.Screen name="approve-forms" options={{ headerShown: false }} />
-              <Stack.Screen name="time-keepings" options={{ headerShown: false }} />
-              <Stack.Screen name="timesheet" options={{ headerShown: false }} />
-              <Stack.Screen
-                name="profile/my-profile"
-                options={{
-                  title: "Hồ sơ cá nhân",
-                  headerShown: true,
-                  headerStyle: {
-                    backgroundColor: "#0B3A82",
-                  },
-                  headerTintColor: "white",
-                  headerTitleAlign: "center",
-                  headerTitle: (props) => (
-                    <NunitoText type="heading3" style={{ color: props.tintColor }}>
-                      {props.children}
-                    </NunitoText>
-                  ),
-                }}
-              />
+        <PaperProvider theme={reactNativePaperCustomTheme}>
+          <SessionProvider>
+            <SocketProvider>
+              <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="forms" options={{ headerShown: false }} />
+                <Stack.Screen name="settings" options={{ headerShown: false }} />
+                <Stack.Screen name="approve-forms" options={{ headerShown: false }} />
+                <Stack.Screen name="time-keepings" options={{ headerShown: false }} />
+                <Stack.Screen name="timesheet" options={{ headerShown: false }} />
+                <Stack.Screen
+                  name="profile/my-profile"
+                  options={{
+                    title: "Hồ sơ cá nhân",
+                    headerShown: true,
+                    headerStyle: {
+                      backgroundColor: "#0B3A82",
+                    },
+                    headerTintColor: "white",
+                    headerTitleAlign: "center",
+                    headerTitle: (props) => (
+                      <NunitoText type="heading3" style={{ color: props.tintColor }}>
+                        {props.children}
+                      </NunitoText>
+                    ),
+                  }}
+                />
 
-              <Stack.Screen
-                name="notification/noti"
-                options={{
-                  title: "Thông báo",
-                  headerShown: true,
-                  headerStyle: {
-                    backgroundColor: "#0B3A82",
-                  },
-                  headerTintColor: "white",
-                  headerTitleAlign: "center",
-                  headerTitle: (props) => (
-                    <NunitoText type="heading3" style={{ color: props.tintColor }}>
-                      {props.children}
-                    </NunitoText>
-                  ),
-                }}
-              />
-            </Stack>
-          </SocketProvider>
-        </SessionProvider>
+                <Stack.Screen
+                  name="notification/noti"
+                  options={{
+                    title: "Thông báo",
+                    headerShown: true,
+                    headerStyle: {
+                      backgroundColor: "#0B3A82",
+                    },
+                    headerTintColor: "white",
+                    headerTitleAlign: "center",
+                    headerTitle: (props) => (
+                      <NunitoText type="heading3" style={{ color: props.tintColor }}>
+                        {props.children}
+                      </NunitoText>
+                    ),
+                  }}
+                />
+              </Stack>
+            </SocketProvider>
+          </SessionProvider>
+        </PaperProvider>
       </ThemeProvider>
     </RootSiblingParent>
   );
---
+++ configs/rnp-custom-theme.ts
@@ -0,0 +1,11 @@
+import { MD3LightTheme as DefaultTheme } from "react-native-paper";
+
+export const reactNativePaperCustomTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#0B3A82",
+    secondary: "#FF9C01",
+    tertiary: "#9C27B0",
+  },
+};
