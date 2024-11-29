d35646e22773398f8a5ac103814178d68f2bfd83
giangann <70595717+giangann@users.noreply.github.com>
2024-11-29 21:46:42 +0700

    weekCalendar define type

--- api/timesheet/type.ts
+++ api/timesheet/type.ts
@@ -13,3 +13,49 @@ export type TMonthTimesheetListParams = {
   month: string | number;
   year: string | number;
 };
+
+export type TWeekCalendar = {
+  id: number;
+  startDate: string;
+  endDate: string;
+  title: string;
+  description: string;
+  isAllDay: boolean;
+};
+
+export type TWeekCalendarCreate = {
+  startDate: string;
+  endDate: string;
+  title: string;
+  description: string;
+  isAllDay: boolean;
+  userIds: number[];
+};
+
+export type TWeekCalendarDetail = {
+  id: number;
+  startDate: string;
+  endDate: string;
+  title: string;
+  description: string;
+  isAllDay: boolean;
+  users: TWeekCalendarUser[];
+};
+
+export type TWeekCalendarUser = {
+  id: number;
+  name: string;
+  email: string | null;
+  address: string | null;
+  phone: string | null;
+  identifyCard: string;
+  roleName: string;
+  roleCode: string;
+  isActive: boolean | null;
+  team: {
+    id: number;
+    name: string;
+    code: string |null;
+    hotline: string |null;
+  };
+};
---
+++ api/timesheet/weekCalendar.ts
@@ -0,0 +1,16 @@
+export async function fetchWeekCalendar(session: string | undefined | null) {
+  const token = `Bearer ${session}`;
+
+  const baseUrl = "https://proven-incredibly-redbird.ngrok-free.app/api/v1";
+  const endpoint = "/week-calendars";
+  const url = `${baseUrl}${endpoint}`;
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: { "Content-Type": "application/json", Authorization: token },
+    credentials: "include",
+  });
+  const responseJson = await response.json();
+
+  return responseJson;
+}
