8ce0d73a18942c793a02a2a45f541b14c6de762b
giangann <70595717+giangann@users.noreply.github.com>
2024-11-29 12:58:14 +0700

    try to fix router navigate bug ios

--- app/timesheet/_layout.tsx
+++ app/timesheet/_layout.tsx
@@ -1,7 +1,13 @@
+import { EditButton } from "@/components/button";
 import { NunitoText } from "@/components/text/NunitoText";
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 
 export default function TimeSheetLayout() {
+  const router = useRouter();
+
+  const customGoBackWeekCalendar = () => {
+    router.navigate("/timesheet");
+  };
   return (
     <Stack
       screenOptions={{
@@ -19,8 +25,10 @@ export default function TimeSheetLayout() {
     >
       <Stack.Screen name="my-timesheet/index" options={{ title: "Bảng chấm công của tôi" }} />
       <Stack.Screen name="my-emp-owt/index" options={{ title: "Bảng ngoài giờ của tôi" }} />
-      <Stack.Screen name="week-calendar" options={{ title: "Lịch công tác" }} />
-
+      <Stack.Screen
+        name="week-calendar"
+        options={{ title: "Lịch công tác", headerRight: () => <EditButton isEdit={true} onToggleEdit={customGoBackWeekCalendar} /> }}
+      />
     </Stack>
   );
 }
