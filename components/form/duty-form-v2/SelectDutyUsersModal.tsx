5d047cda6f40110961300256b95a5af08741f317
giangann <gta007md@gmail.com>
2025-01-07 15:55:24 +0700

    chore: prettier formated

--- components/form/duty-form-v2/SelectDutyUsersModal.tsx
+++ components/form/duty-form-v2/SelectDutyUsersModal.tsx
@@ -1,4 +1,7 @@
-import { TDutySuggestedUser, TDutySuggestedUserFilterParams } from "@/api/form/types";
+import {
+  TDutySuggestedUser,
+  TDutySuggestedUserFilterParams,
+} from "@/api/form/types";
 import { MyIconButton } from "@/components/button";
 import { Delayed } from "@/components/Delayed";
 import { NunitoText } from "@/components/text/NunitoText";
@@ -19,239 +22,288 @@ type TFilterCheckStatus = "checked" | "all";
 type TUserWithCheckStatus = TDutySuggestedUser & { isChecked: boolean };
 
 export const SelectDutyUsersModal: React.FC = memo(() => {
-    const { selectingDutyType, setOpenSelectUsersModal, setOpenSelectDutyTypeModal, useFieldArrayReturn, useFormReturn } = useDutyFormCreateProvider()
-    const { users: suggestedUsers, isLoading: isFetchingUsers, onFetchDutySuggestedUsers } = useSuggestDutyUsers();
+  const {
+    selectingDutyType,
+    setOpenSelectUsersModal,
+    setOpenSelectDutyTypeModal,
+    useFieldArrayReturn,
+    useFormReturn,
+  } = useDutyFormCreateProvider();
+  const {
+    users: suggestedUsers,
+    isLoading: isFetchingUsers,
+    onFetchDutySuggestedUsers,
+  } = useSuggestDutyUsers();
 
-    const [filterCheckStatus, setFilterCheckStatus] = useState<TFilterCheckStatus>("all");
-    const [text, setText] = useState("");
+  const [filterCheckStatus, setFilterCheckStatus] =
+    useState<TFilterCheckStatus>("all");
+  const [text, setText] = useState("");
 
-    const selectedUserIdsRef = useRef<number[]>(useFieldArrayReturn?.fields?.filter((field) => field.dutyTypeId === selectingDutyType?.id)?.[0]?.dutyTypeUsers?.map((user) => user.id) ?? [])
+  const selectedUserIdsRef = useRef<number[]>(
+    useFieldArrayReturn?.fields
+      ?.filter((field) => field.dutyTypeId === selectingDutyType?.id)?.[0]
+      ?.dutyTypeUsers?.map((user) => user.id) ?? []
+  );
 
-    const usersWithCheckStatus: TUserWithCheckStatus[] = useMemo(
-        () => suggestedUsers.map((user) => ({ ...user, isChecked: selectedUserIdsRef.current.includes(user.id) ? true : false })),
-        [suggestedUsers, selectedUserIdsRef]
+  const usersWithCheckStatus: TUserWithCheckStatus[] = useMemo(
+    () =>
+      suggestedUsers.map((user) => ({
+        ...user,
+        isChecked: selectedUserIdsRef.current.includes(user.id) ? true : false,
+      })),
+    [suggestedUsers, selectedUserIdsRef]
+  );
+  const selectedUsers = useMemo(
+    () => usersWithCheckStatus.filter((user) => user.isChecked),
+    [usersWithCheckStatus]
+  );
+
+  const users: TDutyFormAttendanceInfo[] = useMemo(
+    () => (filterCheckStatus === "all" ? usersWithCheckStatus : selectedUsers),
+    [selectedUsers, usersWithCheckStatus, filterCheckStatus]
+  );
+
+  const searchedUsers = useMemo(() => {
+    return users.filter((user) =>
+      user.name.toLowerCase().includes(text.toLowerCase())
     );
-    const selectedUsers = useMemo(() => usersWithCheckStatus.filter((user) => user.isChecked), [usersWithCheckStatus])
+  }, [text, users]);
 
-    const users: TDutyFormAttendanceInfo[] = useMemo(
-        () => (filterCheckStatus === "all" ? usersWithCheckStatus : selectedUsers),
-        [selectedUsers, usersWithCheckStatus, filterCheckStatus]
+  const onCloseBtnPressed = useCallback(() => {
+    setOpenSelectUsersModal(false);
+    setOpenSelectDutyTypeModal(false);
+  }, [setOpenSelectUsersModal, setOpenSelectDutyTypeModal]);
+
+  const onSaveBtnPressed = useCallback(() => {
+    const selectedIds = selectedUserIdsRef.current;
+
+    // close 2 modal
+    setOpenSelectUsersModal(false);
+    setOpenSelectDutyTypeModal(false);
+
+    // update fields from state to form
+    const dutyTypes = useFieldArrayReturn?.fields ?? [];
+    const selectingDutyTypeId = selectingDutyType?.id;
+    const currentDutyType = dutyTypes.filter(
+      (field, index) => field.dutyTypeId === selectingDutyTypeId
+    )?.[0];
+
+    // take dutyTypeUsers
+    const dutyTypeUsers = users.filter((user) => selectedIds.includes(user.id));
+
+    if (!currentDutyType) {
+      useFieldArrayReturn?.append({
+        dutyTypeId: selectingDutyType!.id,
+        dutyTypeName: selectingDutyType!.name,
+        dutyTypeUsers: dutyTypeUsers,
+      });
+    } else {
+      // find index
+      const fieldIndex =
+        useFieldArrayReturn?.fields?.findIndex(
+          (field) => field.dutyTypeId === selectingDutyTypeId
+        ) ?? -1;
+
+      useFieldArrayReturn?.update(fieldIndex, {
+        dutyTypeId: selectingDutyType!.id,
+        dutyTypeName: selectingDutyType!.name,
+        dutyTypeUsers: dutyTypeUsers,
+      });
+    }
+  }, [selectedUserIdsRef, selectingDutyType, useFieldArrayReturn, users]);
+
+  const onSelectUser = useCallback(
+    (user: TDutyFormAttendanceInfo) => {
+      const selectedIds: number[] = selectedUserIdsRef.current;
+      const hasUser = selectedIds.includes(user.id);
+
+      // If already have, remove. Else add new
+      if (hasUser)
+        selectedUserIdsRef.current = selectedIds.filter(
+          (selectedId) => selectedId !== user.id
+        );
+      else selectedUserIdsRef.current = [...selectedIds, user.id];
+    },
+    [selectedUserIdsRef]
+  );
+
+  const onFilterApply = useCallback(
+    (params: TDutySuggestedUserFilterParams) => {
+      onFetchDutySuggestedUsers({ page: 0, size: 50 }, params);
+    },
+    []
+  );
+
+  useEffect(() => {
+    onFetchDutySuggestedUsers(
+      { page: 0, size: 50 },
+      _defaultFilterParams(
+        selectingDutyType!.id,
+        moment(useFormReturn?.getValues("date")).format("YYYY-MM-DD")
+      )
     );
+  }, [onFetchDutySuggestedUsers, selectingDutyType]);
 
-    const searchedUsers = useMemo(() => {
-        return users.filter((user) => user.name.toLowerCase().includes(text.toLowerCase()));
-    }, [text, users]);
+  return (
+    <Modal transparent animationType="slide">
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.modalContainer}>
+          <Delayed waitBeforeShow={100}>
+            <View>
+              {/* Title */}
+              <View style={styles.modalTitleWrapper}>
+                <NunitoText type="heading2" style={styles.modalTitle}>
+                  {selectingDutyType?.name} - chọn người tham gia
+                </NunitoText>
+                <MyIconButton
+                  onPressed={onCloseBtnPressed}
+                  iconElement={
+                    <AntDesign name="close" size={20} color="black" />
+                  }
+                  paddingSize={16}
+                  extendStyles={{ position: "absolute", right: 0, top: 0 }}
+                />
+              </View>
 
-    const onCloseBtnPressed = useCallback(() => {
-        setOpenSelectUsersModal(false)
-        setOpenSelectDutyTypeModal(false)
-    }, [setOpenSelectUsersModal, setOpenSelectDutyTypeModal])
+              {/* Content */}
+              <FlatList
+                data={searchedUsers}
+                renderItem={({ item: user }) => (
+                  <SuggestUserCard
+                    isChecked={user.isChecked}
+                    key={user.id}
+                    user={user}
+                    onSelectUser={onSelectUser}
+                  />
+                )}
+                ListEmptyComponent={
+                  isFetchingUsers ? (
+                    <SkeletonRectangleLoader height={400} />
+                  ) : (
+                    <NoData message="Không có nhân viên thuộc loại trực" />
+                  )
+                }
+                ListHeaderComponent={
+                  <View style={{ gap: 2 }}>
+                    <View style={styles.filterContainer}>
+                      <View style={styles.filterItem}>
+                        <SegmentedButtons
+                          value={filterCheckStatus}
+                          onValueChange={(value: string) => {
+                            console.log({ value });
+                            setFilterCheckStatus(value as TFilterCheckStatus);
+                          }}
+                          buttons={[
+                            { value: "all", label: "Tất cả" },
+                            { value: "checked", label: "Đã chọn" },
+                          ]}
+                          style={{ marginLeft: -1 }}
+                        />
+                      </View>
 
-    const onSaveBtnPressed = useCallback(() => {
-        const selectedIds = selectedUserIdsRef.current
-
-        // close 2 modal
-        setOpenSelectUsersModal(false)
-        setOpenSelectDutyTypeModal(false)
-
-        // update fields from state to form
-        const dutyTypes = useFieldArrayReturn?.fields ?? []
-        const selectingDutyTypeId = selectingDutyType?.id
-        const currentDutyType = dutyTypes.filter((field, index) => field.dutyTypeId === selectingDutyTypeId)?.[0]
-
-        // take dutyTypeUsers
-        const dutyTypeUsers = users.filter((user) => selectedIds.includes(user.id))
-
-
-        if (!currentDutyType) {
-            useFieldArrayReturn?.append({
-                dutyTypeId: selectingDutyType!.id,
-                dutyTypeName: selectingDutyType!.name,
-                dutyTypeUsers: dutyTypeUsers
-            })
-        } else {
-            // find index
-            const fieldIndex = useFieldArrayReturn?.fields?.findIndex((field) => field.dutyTypeId === selectingDutyTypeId) ?? -1
-
-            useFieldArrayReturn?.update(fieldIndex, {
-                dutyTypeId: selectingDutyType!.id,
-                dutyTypeName: selectingDutyType!.name,
-                dutyTypeUsers: dutyTypeUsers
-            })
-        }
-    }, [selectedUserIdsRef, selectingDutyType, useFieldArrayReturn, users])
-
-    const onSelectUser = useCallback((user: TDutyFormAttendanceInfo) => {
-        const selectedIds: number[] = selectedUserIdsRef.current
-        const hasUser = selectedIds.includes(user.id)
-
-        // If already have, remove. Else add new
-        if (hasUser) selectedUserIdsRef.current = selectedIds.filter((selectedId) => selectedId !== user.id)
-        else selectedUserIdsRef.current = [...selectedIds, user.id]
-    }, [selectedUserIdsRef])
-
-    const onFilterApply = useCallback((params: TDutySuggestedUserFilterParams) => {
-        onFetchDutySuggestedUsers({ page: 0, size: 50 }, params);
-      }, []);
-    
-
-    useEffect(() => {
-        onFetchDutySuggestedUsers({ page: 0, size: 50 }, _defaultFilterParams(selectingDutyType!.id, moment(useFormReturn?.getValues('date')).format("YYYY-MM-DD")));
-    }, [onFetchDutySuggestedUsers, selectingDutyType]);
-
-
-    return (
-        <Modal transparent animationType="slide">
-            <SafeAreaView style={{ flex: 1 }}>
-                <View style={styles.modalContainer}>
-                    <Delayed waitBeforeShow={100}>
-                        <View>
-                            {/* Title */}
-                            <View style={styles.modalTitleWrapper}>
-                                <NunitoText type="heading2" style={styles.modalTitle}>
-                                    {selectingDutyType?.name} - chọn người tham gia
-                                </NunitoText>
-                                <MyIconButton
-                                    onPressed={onCloseBtnPressed}
-                                    iconElement={
-                                        <AntDesign name="close" size={20} color="black" />
-                                    }
-                                    paddingSize={16}
-                                    extendStyles={{ position: "absolute", right: 0, top: 0 }}
-                                />
-                            </View>
-
-                            {/* Content */}
-                            <FlatList
-                                data={searchedUsers}
-                                renderItem={({ item: user }) => (
-                                    <SuggestUserCard
-                                        isChecked={user.isChecked}
-                                        key={user.id}
-                                        user={user}
-                                        onSelectUser={onSelectUser}
-                                    />
-                                )}
-
-                                ListEmptyComponent={
-                                    isFetchingUsers ? <SkeletonRectangleLoader height={400} /> : <NoData message="Không có nhân viên thuộc loại trực" />
-                                }
-                                ListHeaderComponent={
-                                    <View style={{ gap: 2 }}>
-                                      <View style={styles.filterContainer}>
-                                        <View style={styles.filterItem}>
-                                          <SegmentedButtons
-                                            value={filterCheckStatus}
-                                            onValueChange={(value: string) => {
-                                              console.log({ value });
-                                              setFilterCheckStatus(value as TFilterCheckStatus);
-                                            }}
-                                            buttons={[
-                                              { value: "all", label: "Tất cả" },
-                                              { value: "checked", label: "Đã chọn" },
-                                            ]}
-                                            style={{ marginLeft: -1 }}
-                                          />
-                                        </View>
-                  
-                                        <FilterUserForm
-                                          onApply={onFilterApply}
-                                          defaultFilterParmas={_defaultFilterParams(selectingDutyType!.id, moment(useFormReturn?.getValues('date')).format("YYYY-MM-DD"))}
-                                        />
-                                      </View>
-                                      {/* Search */}
-                                      <TextInput
-                                        style={{ height: 36 }}
-                                        mode="outlined"
-                                        label="Tên thành viên"
-                                        value={text}
-                                        onChangeText={(text) => setText(text)}
-                                        placeholder="nhập tên để tìm kiếm "
-                                      />
-                                    </View>
-                                  }
-                  
-                                contentContainerStyle={styles.modalContentContainer}
-                            />
-
-                        </View>
-                    </Delayed>
-                    {/* Save button */}
-                    <AnimatedFAB
-                        icon={"plus"}
-                        label={"Label"}
-                        extended={false}
-                        onPress={onSaveBtnPressed}
-                        visible={true}
-                        animateFrom={"right"}
-                        iconMode={"static"}
-                        style={[styles.fabStyle]}
+                      <FilterUserForm
+                        onApply={onFilterApply}
+                        defaultFilterParmas={_defaultFilterParams(
+                          selectingDutyType!.id,
+                          moment(useFormReturn?.getValues("date")).format(
+                            "YYYY-MM-DD"
+                          )
+                        )}
+                      />
+                    </View>
+                    {/* Search */}
+                    <TextInput
+                      style={{ height: 36 }}
+                      mode="outlined"
+                      label="Tên thành viên"
+                      value={text}
+                      onChangeText={(text) => setText(text)}
+                      placeholder="nhập tên để tìm kiếm "
                     />
-                </View>
-            </SafeAreaView>
-        </Modal>
-
-    )
-})
+                  </View>
+                }
+                contentContainerStyle={styles.modalContentContainer}
+              />
+            </View>
+          </Delayed>
+          {/* Save button */}
+          <AnimatedFAB
+            icon={"plus"}
+            label={"Label"}
+            extended={false}
+            onPress={onSaveBtnPressed}
+            visible={true}
+            animateFrom={"right"}
+            iconMode={"static"}
+            style={[styles.fabStyle]}
+          />
+        </View>
+      </SafeAreaView>
+    </Modal>
+  );
+});
 const styles = StyleSheet.create({
-    modalContainer: {
-        flex: 1,
-        width: "100%",
-        backgroundColor: "white",
-        position: "relative",
-    },
+  modalContainer: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "white",
+    position: "relative",
+  },
 
-    modalTitleWrapper: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "flex-start",
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-    },
+  modalTitleWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
 
-    modalTitle: {
-        color: "black",
-    },
+  modalTitle: {
+    color: "black",
+  },
 
-    modalContentContainer: {
-        paddingHorizontal: 20,
-        paddingVertical: 16,
-        gap: 16,
-    },
+  modalContentContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 16,
+    gap: 16,
+  },
 
-    filterContainer: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        gap: 10,
-    },
+  filterContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: 10,
+  },
 
-    filterItem: {
-        flexGrow: 1,
-    },
+  filterItem: {
+    flexGrow: 1,
+  },
 
-    fabStyle: {
-        bottom: 16,
-        right: 16,
-        position: "absolute",
-    },
+  fabStyle: {
+    bottom: 16,
+    right: 16,
+    position: "absolute",
+  },
 });
 
-const _defaultFilterParams = (dutyTypeId: number, dutyDate: string): TDutySuggestedUserFilterParams => ({
-    date: dutyDate,
-    startDate: moment().startOf("year").format("YYYY-MM-DD"),
-    endDate: moment().endOf("year").format("YYYY-MM-DD"),
-    dutyTypeId,
-    sort: sortOpts[0].value,
+const _defaultFilterParams = (
+  dutyTypeId: number,
+  dutyDate: string
+): TDutySuggestedUserFilterParams => ({
+  date: dutyDate,
+  startDate: moment().startOf("year").format("YYYY-MM-DD"),
+  endDate: moment().endOf("year").format("YYYY-MM-DD"),
+  dutyTypeId,
+  sort: sortOpts[0].value,
 });
 
 export const sortOpts = [
-    {
-        label: "Số lần trực tăng dần",
-        value: "numOnDuty,asc",
-    },
-    {
-        label: "Số lần trực giảm dần",
-        value: "numOnDuty,desc",
-    },
+  {
+    label: "Số lần trực tăng dần",
+    value: "numOnDuty,asc",
+  },
+  {
+    label: "Số lần trực giảm dần",
+    value: "numOnDuty,desc",
+  },
 ];
--- components/form/duty-form-v2/SuggestUserCard.tsx
+++ components/form/duty-form-v2/SuggestUserCard.tsx
@@ -1,51 +1,68 @@
 import { TDutyFormAttendanceInfo } from "@/types";
 import { memo, useCallback, useState } from "react";
 import { StyleSheet, View } from "react-native";
-import { Avatar, Card, Checkbox, Text, TouchableRipple } from "react-native-paper";
+import {
+  Avatar,
+  Card,
+  Checkbox,
+  Text,
+  TouchableRipple,
+} from "react-native-paper";
 
 type SuggestUserItemProps = {
-    user: TDutyFormAttendanceInfo;
-    isChecked: boolean;
-    onSelectUser: (user: TDutyFormAttendanceInfo) => void
+  user: TDutyFormAttendanceInfo;
+  isChecked: boolean;
+  onSelectUser: (user: TDutyFormAttendanceInfo) => void;
 };
-export const SuggestUserCard: React.FC<SuggestUserItemProps> = memo(({ user, isChecked, onSelectUser }) => {
-    const [checked, setChecked] = useState(isChecked)
+export const SuggestUserCard: React.FC<SuggestUserItemProps> = memo(
+  ({ user, isChecked, onSelectUser }) => {
+    const [checked, setChecked] = useState(isChecked);
 
-    const LeftContent = (props: any) => <Avatar.Icon {...props} icon="account" />;
-
+    const LeftContent = (props: any) => (
+      <Avatar.Icon {...props} icon="account" />
+    );
 
     const onPressUserCard = useCallback(() => {
-        onSelectUser(user)
-        setChecked((prev) => !prev)
+      onSelectUser(user);
+      setChecked((prev) => !prev);
     }, [user, setChecked, onSelectUser]);
 
     return (
-        <Card style={styles.card}>
-            <TouchableRipple borderless rippleColor="rgba(0, 0, 0, .32)" onPress={onPressUserCard}>
-                <View style={styles.userCardWrapper}>
-                    <Card.Title title={user.name} subtitle={`P. ${user.teamName}`} left={LeftContent} />
-                    <Card.Content>
-                        <Text variant="bodyMedium">Chức vụ: {user.roleName}</Text>
-                        <Text variant="bodyMedium">Số lần trực: {user.numOnDuty}</Text>
-                    </Card.Content>
-                    <View style={styles.userCheckboxWrapper}>
-                        <Checkbox status={checked ? "checked" : "unchecked"} />
-                    </View>
-                </View>
-            </TouchableRipple>
-        </Card>
+      <Card style={styles.card}>
+        <TouchableRipple
+          borderless
+          rippleColor="rgba(0, 0, 0, .32)"
+          onPress={onPressUserCard}
+        >
+          <View style={styles.userCardWrapper}>
+            <Card.Title
+              title={user.name}
+              subtitle={`P. ${user.teamName}`}
+              left={LeftContent}
+            />
+            <Card.Content>
+              <Text variant="bodyMedium">Chức vụ: {user.roleName}</Text>
+              <Text variant="bodyMedium">Số lần trực: {user.numOnDuty}</Text>
+            </Card.Content>
+            <View style={styles.userCheckboxWrapper}>
+              <Checkbox status={checked ? "checked" : "unchecked"} />
+            </View>
+          </View>
+        </TouchableRipple>
+      </Card>
     );
-});
+  }
+);
 
 const styles = StyleSheet.create({
-    card: {},
-    userCardWrapper: {
-        position: "relative",
-        paddingBottom: 16,
-    },
-    userCheckboxWrapper: {
-        position: "absolute",
-        right: 20,
-        top: 20,
-    },
+  card: {},
+  userCardWrapper: {
+    position: "relative",
+    paddingBottom: 16,
+  },
+  userCheckboxWrapper: {
+    position: "absolute",
+    right: 20,
+    top: 20,
+  },
 });
