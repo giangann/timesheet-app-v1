dd249a719c5429bc59e9cff2caf7f8b9684e7aa0
giangann <gta007md@gmail.com>
2025-01-08 10:15:39 +0700

    weekCalendar orientation only on android

--- components/timesheet/Header.tsx
+++ components/timesheet/Header.tsx
@@ -1,14 +1,20 @@
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import type { DrawerNavigationProp } from '@react-navigation/drawer';
-import { DrawerActions, useTheme } from '@react-navigation/native';
-import { useNavigation } from 'expo-router';
-import type { FC } from 'react';
-import React, { useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import type { SharedValue } from 'react-native-reanimated';
-import { runOnJS, useAnimatedReaction } from 'react-native-reanimated';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { ChooseOrientation } from './ChooseOrientation';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import type { DrawerNavigationProp } from "@react-navigation/drawer";
+import { DrawerActions, useTheme } from "@react-navigation/native";
+import { useNavigation } from "expo-router";
+import type { FC } from "react";
+import React, { useState } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import type { SharedValue } from "react-native-reanimated";
+import { runOnJS, useAnimatedReaction } from "react-native-reanimated";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { ChooseOrientation } from "./ChooseOrientation";
 
 interface HeaderProps {
   currentDate: SharedValue<string>;
@@ -25,13 +31,13 @@ const Header: FC<HeaderProps> = ({
 }) => {
   const theme = useTheme();
   const { top: safeTop } = useSafeAreaInsets();
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
   const navigation = useNavigation<DrawerNavigationProp<any>>();
 
   const updateTitle = (date: string) => {
-    const formatted = new Date(date).toLocaleDateString('vi-VN', {
-      year: 'numeric',
-      month: 'long',
+    const formatted = new Date(date).toLocaleDateString("vi-VN", {
+      year: "numeric",
+      month: "long",
     });
     setTitle(formatted);
   };
@@ -53,11 +59,13 @@ const Header: FC<HeaderProps> = ({
       style={[
         styles.header,
         { paddingTop: safeTop + 16, backgroundColor: theme.colors.card },
-      ]}>
+      ]}
+    >
       <TouchableOpacity
         activeOpacity={0.6}
         style={styles.menuBtn}
-        onPress={_onPressMenu}>
+        onPress={_onPressMenu}
+      >
         <MaterialCommunityIcons
           name="menu"
           size={24}
@@ -84,19 +92,20 @@ const Header: FC<HeaderProps> = ({
         <Text style={[styles.headerTitle, { color: theme.colors.text }]}>
           {title}
         </Text>
-        <ChooseOrientation/>
+
+        {Platform.OS === "android" && <ChooseOrientation />}
 
         <TouchableOpacity
           hitSlop={{ top: 8, right: 8, bottom: 8, left: 8 }}
           activeOpacity={0.6}
-          onPress={onPressToday}>
+          onPress={onPressToday}
+        >
           <MaterialCommunityIcons
             name="calendar"
             size={24}
             color={theme.colors.text}
           />
         </TouchableOpacity>
-
       </View>
     </View>
   );
@@ -105,22 +114,22 @@ const Header: FC<HeaderProps> = ({
 export default Header;
 const styles = StyleSheet.create({
   header: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     paddingBottom: 16,
     paddingHorizontal: 12,
   },
-  menuBtn: { width: 40, alignItems: 'center' },
+  menuBtn: { width: 40, alignItems: "center" },
   headerRightContent: {
-    flexDirection: 'row',
+    flexDirection: "row",
     flexGrow: 1,
-    alignItems: 'center',
+    alignItems: "center",
     flexShrink: 1,
   },
-  headerTitle: { flexGrow: 1, flexShrink: 1, fontSize: 16, fontWeight: '500' },
+  headerTitle: { flexGrow: 1, flexShrink: 1, fontSize: 16, fontWeight: "500" },
   navigation: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 4,
     marginHorizontal: 8,
   },
