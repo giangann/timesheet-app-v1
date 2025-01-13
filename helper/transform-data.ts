f8001a35341caec9ffbdb2cf1b6b8d8f48e13dcb
giangann <gta007md@gmail.com>
2025-01-13 22:14:28 +0700

    update calendar for dutyFOrm

--- helper/transform-data.ts
+++ helper/transform-data.ts
@@ -4,10 +4,16 @@ import { EventItem } from "@howljs/calendar-kit";
 import { formatToISOWithMilliseconds } from "./date";
 import moment from "moment";
 import { TWeekCalendarCreateFormFieldsUser } from "@/types";
-import { TDutyForm, TDutyFormDetailDutyType, TLeaveForm } from "@/api/form/types";
+import {
+  TDutyForm,
+  TDutyFormDetailDutyType,
+  TLeaveForm,
+} from "@/api/form/types";
 import { arrayStringToString, combineDateAndTimeToDateObject } from "./common";
 
-export function weekCalendarToEventItems(weekCalendars: TWeekCalendar[]): EventItem[] {
+export function weekCalendarToEventItems(
+  weekCalendars: TWeekCalendar[]
+): EventItem[] {
   const res: EventItem[] = [];
 
   weekCalendars.forEach((calendar) => {
@@ -47,10 +53,24 @@ export function dutyFormToEventItems(dutyForms: TDutyForm[]): EventItem[] {
   dutyForms.forEach((form) => {
     const eventItem: EventItem = {
       id: `${EVENT_ITEM_PREFIX.DUTY_FORM}-${form.id}`,
-      title: `${form.salaryCoefficientTypeName} (x${form.salaryCoefficient.toFixed(2)}); ${form.dutyTypeNames.join("+ ")}; ${arrayStringToString(form.userNames.map((user) => user.name))}`,
+      title: `${
+        form.salaryCoefficientTypeName
+      } (x${form.salaryCoefficient.toFixed(2)}); ${dutyTypeToEventTitle(
+        form.dutyTypes
+      )};`,
       color: EVENT_COLOR[EVENT_ITEM_PREFIX.DUTY_FORM],
-      start: { dateTime: combineDateAndTimeToDateObject(form.date, form.startTime).toISOString() },
-      end: { dateTime: combineDateAndTimeToDateObject(form.date, form.endTime).toISOString() },
+      start: {
+        dateTime: combineDateAndTimeToDateObject(
+          form.date,
+          form.startTime
+        ).toISOString(),
+      },
+      end: {
+        dateTime: combineDateAndTimeToDateObject(
+          form.date,
+          form.endTime
+        ).toISOString(),
+      },
     };
 
     res.push(eventItem);
@@ -65,7 +85,9 @@ export function leaveFormToEventItems(leaveForm: TLeaveForm[]): EventItem[] {
   leaveForm.forEach((form) => {
     const eventItem: EventItem = {
       id: `${EVENT_ITEM_PREFIX.LEAVE_FORM}-${form.id}`,
-      title: `${form.leaveFormTypeName} - ${form.userName}, P.${form.userTeam.name} ${form.note ?? `- ${form.note}`}`,
+      title: `${form.leaveFormTypeName} - ${form.userName}, P.${
+        form.userTeam.name
+      } ${form.note ?? `- ${form.note}`}`,
       color: EVENT_COLOR[EVENT_ITEM_PREFIX.LEAVE_FORM],
       start: { date: moment(form.startDate).format("YYYY-MM-DD") },
       end: { date: moment(form.endDate).format("YYYY-MM-DD") },
@@ -76,7 +98,9 @@ export function leaveFormToEventItems(leaveForm: TLeaveForm[]): EventItem[] {
 
   return res;
 }
-export function weekCalendarUsersToUserFields(weekCalendarUsers: TWeekCalendarDetail["users"]): TWeekCalendarCreateFormFieldsUser[] {
+export function weekCalendarUsersToUserFields(
+  weekCalendarUsers: TWeekCalendarDetail["users"]
+): TWeekCalendarCreateFormFieldsUser[] {
   const res: TWeekCalendarCreateFormFieldsUser[] = [];
 
   weekCalendarUsers.forEach((user) => {
@@ -91,16 +115,33 @@ export function weekCalendarUsersToUserFields(weekCalendarUsers: TWeekCalendarDe
   return res;
 }
 
-export function dutyTypesToUsers(dutyTypes:TDutyFormDetailDutyType[] ):TDutyFormDetailDutyType['users'] {
-  const users: TDutyFormDetailDutyType['users'] = []
+export function dutyTypesToUsers(
+  dutyTypes: TDutyFormDetailDutyType[]
+): TDutyFormDetailDutyType["users"] {
+  const users: TDutyFormDetailDutyType["users"] = [];
 
-  dutyTypes.forEach((dtType)=>{
-    users.push(...dtType.users)
-  })
+  dutyTypes.forEach((dtType) => {
+    users.push(...dtType.users);
+  });
 
-  return users
+  return users;
 }
 
-export function dutyTypeUsersToName (dtTypeUsers: TDutyFormDetailDutyType['users']):string{
-  return dtTypeUsers.map((user)=>user.name).join(', ')
+export function dutyTypeUsersToName(
+  dtTypeUsers: TDutyFormDetailDutyType["users"]
+): string {
+  return dtTypeUsers.map((user) => user.name).join(", ");
+}
+
+export function dutyTypeToEventTitle(
+  dutyTypes: TDutyFormDetailDutyType[]
+): string {
+  let res = "";
+
+  dutyTypes.forEach((dutyType) => {
+    res += `• ${dutyType.dutyTypeName} (${dutyTypeUsersToName(
+      dutyType.users
+    )}) `;
+  });
+  return res;
 }
