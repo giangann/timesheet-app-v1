87e0b35b1a416542a082115f78ec3f0243d6f86a
giangann <gta007md@gmail.com>
2025-01-18 17:44:48 +0700

    update dutyform edit by fab

--- app/forms/duty_forms/[id].tsx
+++ app/forms/duty_forms/[id].tsx
@@ -1,23 +1,41 @@
 import { fetchDutyFormDetail } from "@/api/form";
 import { TDutyFormDetail } from "@/api/form/types";
-import { EditButton } from "@/components/button";
+import { EditButton, MyFAB } from "@/components/button";
 import { DutyFormEdit } from "@/components/form";
 import { DutyFormDetail } from "@/components/form/duty-form/DutyFormDetail";
+import { MyModal } from "@/components/MyModal";
 import { NunitoText } from "@/components/text/NunitoText";
+import { FORM_STATUS } from "@/constants/Misc";
 import { useSession } from "@/contexts/ctx";
+import { useDeleteForm } from "@/hooks/form";
 import { MyToast } from "@/ui/MyToast";
-import { useFocusEffect, useLocalSearchParams, useNavigation } from "expo-router";
-import { useCallback, useLayoutEffect, useState } from "react";
+import {
+  useFocusEffect,
+  useLocalSearchParams,
+  useNavigation,
+} from "expo-router";
+import { useCallback, useLayoutEffect, useMemo, useState } from "react";
 import { View } from "react-native";
+import { useTheme } from "react-native-paper";
 
 export default function DetailForm() {
   const [form, setForm] = useState<TDutyFormDetail | null>(null);
   const [edit, setEdit] = useState(false);
+  const [openCfCancelModal, setOpenCfCancelModal] = useState(false);
 
   const navigation = useNavigation();
-  const { session } = useSession();
+  const { session, userInfo } = useSession();
   const local = useLocalSearchParams();
+  const theme = useTheme();
+
+  const { onDelete } = useDeleteForm();
+
   const formId = local.id;
+  const isShowFab = useMemo(
+    () => form?.status !== FORM_STATUS.CANCELED,
+    [form]
+  );
+  const ableToDeleteOrCancel = userInfo?.id === form?.createdUserId;
 
   const toggleEditMode = () => setEdit((prev) => !prev);
 
@@ -30,19 +48,17 @@ export default function DetailForm() {
     }
   };
 
+  const onDeleteForm = useCallback(() => {
+    onDelete(form?.id ?? 0);
+  }, [form]);
+
   useFocusEffect(
     useCallback(() => {
       onFetchDutyFormDetail(formId as string);
     }, [formId, session])
   );
 
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerRight: () => <EditButton isEdit={edit} onToggleEdit={toggleEditMode} />,
-    });
-  }, [edit, toggleEditMode]);
-
-  return (
+    return (
     <>
       {!form && (
         <View>
@@ -53,6 +69,58 @@ export default function DetailForm() {
       {form && !edit && <DutyFormDetail form={form} />}
 
       {form && edit && <DutyFormEdit form={form} />}
+
+      {form && isShowFab && ableToDeleteOrCancel && (
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+          }}
+        >
+          <MyFAB
+            actions={[
+              {
+                icon: "delete",
+                onPress: () => {
+                  console.log("deleted");
+                  setOpenCfCancelModal(true);
+                },
+                style: {
+                  backgroundColor: theme.colors.error,
+                  display: "flex",
+                },
+                color: "white",
+              },
+              {
+                icon: edit ? "eye-arrow-left" : "pencil",
+                onPress: toggleEditMode,
+                style: {
+                  backgroundColor: theme.colors.tertiary,
+                  display: "flex",
+                },
+                color: "white",
+              },
+            ]}
+            closingIcon={"menu"}
+          />
+        </View>
+      )}
+
+      {openCfCancelModal && (
+        <MyModal
+          title={"Xác nhận xóa đơn"}
+          onClose={() => setOpenCfCancelModal(false)}
+          cb={onDeleteForm}
+          modalProps={{ animationType: "slide", transparent: true }}
+        >
+          <View>
+            <NunitoText type="body3">Bạn có chắc xóa đơn trực?</NunitoText>
+          </View>
+        </MyModal>
+      )}
     </>
   );
 }
--- components/form/duty-form/DutyFormDetail.tsx
+++ components/form/duty-form/DutyFormDetail.tsx
@@ -1,95 +1,76 @@
 import { TDutyFormDetail } from "@/api/form/types";
-import { MyModal } from "@/components/MyModal";
 import { ViewImageFullScreen } from "@/components/ViewImageFullScreen";
 import { NunitoText } from "@/components/text/NunitoText";
 import { OPACITY_TO_HEX } from "@/constants/Colors";
-import { useSession } from "@/contexts";
-import { useDeleteForm } from "@/hooks/form";
 import { BoxStatus } from "@/ui/BoxStatus";
 import moment from "moment";
-import { memo, useCallback, useState } from "react";
+import { memo } from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
-import { Button, useTheme } from "react-native-paper";
 import { DutyFormDetailDutyTypes } from "./DutyFormDetailDutyTypes";
 
 type DutyFormDetailProps = {
   form: TDutyFormDetail | null;
 };
-export const DutyFormDetail: React.FC<DutyFormDetailProps> = memo(({ form }) => {
-  const [openCfCancelModal, setOpenCfCancelModal] = useState(false);
-  const { userInfo } = useSession();
-  const { onDelete } = useDeleteForm();
-  const theme = useTheme();
+export const DutyFormDetail: React.FC<DutyFormDetailProps> = memo(
+  ({ form }) => {
+    return (
+      <>
+        {form && (
+          <View style={styles.container}>
+            <ScrollView contentContainerStyle={styles.listBox}>
+              <BoxStatus status={form.status} approveDate={form.approveDate} />
+              <Item
+                title="Ngày trực"
+                content={`${moment(form.date).format("DD/MM/YYYY")}`}
+              />
+              <Item
+                title="Giờ trực"
+                content={`${form.startTime} --> ${form.endTime}`}
+              />
 
-  const ableToDeleteOrCancel = userInfo?.id === form?.createdUserId;
+              <Item
+                title="Loại trực"
+                content={
+                  <DutyFormDetailDutyTypes formDutyTypes={form.dutyTypes} />
+                }
+              />
+              <Item
+                title="Loại ngoài giờ"
+                content={`${
+                  form.salaryCoefficientTypeName
+                } (x${form.salaryCoefficient.toFixed(2)})`}
+              />
 
-  const onDeleteForm = useCallback(() => {
-    onDelete(form?.id ?? 0);
-  }, [form]);
+              <Item
+                title="Nội dung công việc"
+                content={form.note ?? "Không có dữ liệu"}
+              />
 
-  return (
-    <>
-      {!form && (
-        <View>
-          <NunitoText>Fetching...</NunitoText>
-        </View>
-      )}
+              {/* Attach Image */}
+              <AttachImageFile path={form?.attachFileUrl} />
+            </ScrollView>
+          </View>
+        )}
+      </>
+    );
+  }
+);
 
-      {form && (
-        <View style={styles.container}>
-          <ScrollView contentContainerStyle={styles.listBox}>
-            <BoxStatus status={form.status} approveDate={form.approveDate} />
-            <Item title="Ngày trực" content={`${moment(form.date).format("DD/MM/YYYY")}`} />
-            <Item title="Giờ trực" content={`${form.startTime} --> ${form.endTime}`} />
-
-            <Item title="Loại trực" content={<DutyFormDetailDutyTypes formDutyTypes={form.dutyTypes} />} />
-            <Item title="Loại ngoài giờ" content={`${form.salaryCoefficientTypeName} (x${form.salaryCoefficient.toFixed(2)})`} />
-
-            <Item title="Nội dung công việc" content={form.note ?? "Không có dữ liệu"} />
-
-            {/* Attach Image */}
-            <AttachImageFile path={form?.attachFileUrl} />
-          </ScrollView>
-
-          {ableToDeleteOrCancel && (
-            <View style={styles.approveContainer}>
-              <Button
-                onPress={() => setOpenCfCancelModal(true)}
-                mode="contained"
-                icon="delete-alert"
-                buttonColor={theme.colors.error}
-                style={styles.buttonContained}
-              >
-                Xóa đơn
-              </Button>
-            </View>
-          )}
-
-          {openCfCancelModal && (
-            <MyModal
-              title={"Xác nhận xóa đơn"}
-              onClose={() => setOpenCfCancelModal(false)}
-              cb={onDeleteForm}
-              modalProps={{ animationType: "slide", transparent: true }}
-            >
-              <View>
-                <NunitoText type="body3">Bạn có chắc xóa đơn trực?</NunitoText>
-              </View>
-            </MyModal>
-          )}
-        </View>
-      )}
-    </>
-  );
-});
-
-const Item = ({ title, content }: { title: string; content: string | React.ReactNode }) => {
+const Item = ({
+  title,
+  content,
+}: {
+  title: string;
+  content: string | React.ReactNode;
+}) => {
   return (
     <View style={styles.item}>
       <NunitoText type="body3" style={{ opacity: 0.5 }}>
         {title}
       </NunitoText>
-      {typeof content === "string" && <NunitoText type="body3">{content}</NunitoText>}
+      {typeof content === "string" && (
+        <NunitoText type="body3">{content}</NunitoText>
+      )}
       {typeof content === "object" && content}
     </View>
   );
@@ -123,22 +104,5 @@ const styles = StyleSheet.create({
     paddingBottom: 6,
     borderBottomWidth: 1,
     borderBottomColor: `#000000${OPACITY_TO_HEX["15"]}`,
-  },
-  approveContainer: {
-    position: "absolute",
-    bottom: 0,
-    // left: 0, // make button on the right
-    right: 0,
-    padding: 16,
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    gap: 6,
-  },
-  buttonItem: {
-    flexGrow: 1,
-  },
-  buttonContained: {
-    height: 44,
   },
 });
